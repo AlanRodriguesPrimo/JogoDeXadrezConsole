4510cdc8c944459e280646fa6a0d80a7652842c4
AlanRodriguesPrimo <alan.rdg.primo@gmail.com>
2024-07-24 14:49:24 -0300

    Criando a classe posição

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
+using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,6 +8,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Posicao P;
+            P = new Posicao(3,4);
+            Console.WriteLine("Posicao: " + P);
         }
     }
 }
