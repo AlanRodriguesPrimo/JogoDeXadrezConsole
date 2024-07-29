a7488d3219e7126b86cbb9a7b79d60d72a573228
AlanRodriguesPrimo <alan.rdg.primo@gmail.com>
2024-07-29 01:21:36 -0300

    começando a colocar peças no tabuleiro

--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -8,9 +8,9 @@ namespace tabuleiro
         public int QteMovimentos { get; protected set; }
         public Tabuleiro Tab { get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.Posicao = posicao;
+            this.Posicao = null;
             this.Tab = tab;
             this.Cor = cor;
             this.QteMovimentos = 0;
